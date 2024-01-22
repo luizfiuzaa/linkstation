12f1893841d39a37324c5086ca661950885913eb
luiafiuzaa <luizffreitas2006@gmail.com>
2024-01-22 15:46:59 -0300

    #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,8 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: 'home',
-    redirectTo: 'home',
+    path: '',
+    redirectTo: '',
     pathMatch: 'full'
   },
 ];
